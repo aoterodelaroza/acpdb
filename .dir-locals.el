0e6ffc33da93c3957880d80b60f4012ef0427970
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2020-10-19 00:49:31 +0200

    update .dir-locals.el with compile command

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ((nil . (
+         (compile-command . "make -C ../build -j 4")
+         (projectile-project-compilation-cmd . "make -C ../build -j 4")
          (eval . (setq-local flycheck-gcc-args
                              (list "-std=c++11")))
 )))
