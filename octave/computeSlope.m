12b016c2080b290997c87c9da656046a14d30dac
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2020-11-07 16:33:46 +0100

    added the updated runlasso script

---
+++ octave/computeSlope.m
@@ -0,0 +1,13 @@
+%% These routines were written by Mark Schmidt (UBC) in 2005. See the 
+%% README file for details.
+
+function [slope_j] = computeSlope(w,lambda,g,threshold)
+% A function that computes the appropriate derivative for non-zero w
+% and points zero-valued w's in the right direction
+%
+% Used by: LassoActiveSet, LassoGrafting, LassoSubGradient
+slope_j = zeros(size(w,1),1);
+
+slope_j(g > lambda) = g(g > lambda) + lambda;
+slope_j(g < -lambda) = g(g < -lambda) - lambda;
+slope_j(abs(w) > threshold) = g(abs(w)>threshold) + lambda*sign(w(abs(w)>threshold));
