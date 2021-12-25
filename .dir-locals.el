1391c9f9b555a9ca52c24fe78253f6f43884c793
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2021-12-25 21:04:31 +0100

    added dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,4 @@
 ((nil . (
          (compile-command . "make -C ../build -j 4")
          (projectile-project-compilation-cmd . "make -C ../build -j 4")
-         (eval . (setq-local flycheck-gcc-args
-                             (list "-std=c++11")))
 )))
