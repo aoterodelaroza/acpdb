7e3f0f2236f4eee495691082da71d909ec0d992d
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2026-04-03 14:02:54 +0200

    example of usage of the ACP class

---
+++ python/ACP_class_examples/make_complete_acps.py
@@ -0,0 +1,14 @@
+# This script shows how to generate a merged ACP from two ACP files.
+
+import sys
+sys.path.append('/home/alberto/git/acpdb/python')
+
+from ACP import *
+from glob import glob
+
+acpbase = ACP.from_file("nonlocal2-cycle0-07.acp")
+
+for acp in glob("*cycle1*acp"):
+    acpnew = acpbase + ACP.from_file(acp)
+    with open("ALL-" + acp,"w") as fout:
+        print(acpnew,file=fout)
